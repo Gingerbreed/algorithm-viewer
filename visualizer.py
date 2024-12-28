5aa7b81ef7ac22441ba9e9da9c99a1fe583c3c27
Morgan Baker <morgan@mbaker.org>
2024-12-28 14:39:30 -0500

    generate list of values

--- visualizer.py
+++ visualizer.py
@@ -10,6 +10,9 @@ class DrawInformation:
     GREY = 128, 128, 128
     BACKGROUND_COLOR = WHITE
 
+    SIDE_PAD = 100
+    TOP_PAD = 150
+
     def __init__(self, width, height, lst):
         self.width = width
         self.height = height
@@ -19,4 +22,20 @@ class DrawInformation:
         self.set_list(lst)
     
     def set_list(self, lst):
-        pass
+        self.lst = lst
+        self.min_val = min(lst)
+        self.max_val = max(lst)
+        
+        self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
+        self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
+        self.start_x = self.SIDE_PAD // 2
+
+
+def generate_starting_list(n, min_val, max_val):
+    lst = []
+
+    for _ in range(n):
+        val = random.randint(min_val, max_val)
+        lst.append(val)
+
+    return lst        
