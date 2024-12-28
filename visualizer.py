5970b89b4b46e756ea9d216bce79f88fda51f90c
Morgan Baker <morgan@mbaker.org>
2024-12-28 15:47:20 -0500

    add bubble sort and re-drawing

--- visualizer.py
+++ visualizer.py
@@ -37,7 +37,6 @@ class DrawInformation:
         self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
         self.start_x = self.SIDE_PAD // 2
 
-
 def generate_starting_list(n, min_val, max_val):
     lst = []
 
@@ -46,6 +45,55 @@ def generate_starting_list(n, min_val, max_val):
         lst.append(val)
 
     return lst        
+
+def draw(draw_info):
+    draw_info.window.fill(draw_info.BACKGROUND_COLOR)
+
+    controls = draw_info.FONT.render("R - Reset | SPACE - Start | A - Ascending | D - Descending", 1, draw_info.BLACK)
+    draw_info.window.blit(controls, (draw_info.width/2 - controls.get_width()/2, 5))
+
+    sorting = draw_info.FONT.render("I - Insertion Sort | B - Bubble Sort", 1, draw_info.BLACK)
+    draw_info.window.blit(sorting, (draw_info.width/2 - controls.get_width()/2, 35))
+
+    draw_list(draw_info)
+    pygame.display.update()
+
+def draw_list(draw_info, color_positions={}, clear_bg = False):
+    lst = draw_info.lst
+    
+    if clear_bg:
+        clear_rect = (draw_info.SIDE_PAD//2, draw_info.TOP_PAD, draw_info.width - draw_info.SIDE_PAD, 
+                        draw_info.height - draw_info.TOP_PAD)
+        pygame.draw.rect(draw_info.window, draw_info.BACKGROUND_COLOR, clear_rect)
+
+    for i, val in enumerate(lst):
+        x = draw_info.start_x + i * draw_info.block_width
+        y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
+
+        color = draw_info.GRADIENTS[i % 3]
+
+        if i in color_positions:
+            color = color_positions[i]
+
+        pygame.draw.rect(draw_info.window, color, (x, y, draw_info.block_width, draw_info.height))
+
+    if clear_bg:
+        pygame.display.update()
+
+def bubble_sort(draw_info, ascending=True):
+    lst = draw_info.lst
+
+    for i in range(len(lst) - 1):
+        for j in range(len(lst) - 1 - i):
+            num1 = lst[j]
+            num2 = lst[j + 1]
+            
+            if (num1 > num2 and ascending) or (num1 < num2 and not ascending):
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+                draw_list(draw_info, {j:draw_info.GREEN, j + 1: draw_info.RED})
+                yield True
+
+    return lst
 
 def main():
     run = True
@@ -89,28 +137,6 @@ def main():
                 ascending = False
 
     pygame.quit()
-
-def draw(draw_info):
-    draw_info.window.fill(draw_info.BACKGROUND_COLOR)
-
-    controls = draw_info.FONT.render("R - Reset | SPACE - Start | A - Ascending | D - Descending", 1, draw_info.BLACK)
-    draw_info.window.blit(controls, (draw_info.width/2 - controls.get_width()/2, 5))
-
-    sorting = draw_info.FONT.render("I - Insertion Sort | B - Bubble Sort", 1, draw_info.BLACK)
-    draw_info.window.blit(controls, (draw_info.width/2 - controls.get_width()/2, 35))
-
-    draw_list(draw_info)
-    pygame.display.update()
-
-def draw_list(draw_info):
-    lst = draw_info.lst
-
-    for i, val in enumerate(lst):
-        x = draw_info.start_x + i * draw_info.block_width
-        y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
-
-        color = draw_info.GRADIENTS[i % 3]
-        pygame.draw.rect(draw_info.window, color, (x, y, draw_info.block_width, draw_info.height))
 
 if __name__ == "__main__":
     main()
