ce36610d2b3553386e9ada5ca0000be9b284e78f
Morgan Baker <morgan@mbaker.org>
2024-12-28 15:05:08 -0500

    drawing rectangle lists

--- visualizer.py
+++ visualizer.py
@@ -7,8 +7,13 @@ class DrawInformation:
     WHITE = 255, 255, 255
     GREEN = 0, 255, 0
     RED = 255, 0, 0
-    GREY = 128, 128, 128
     BACKGROUND_COLOR = WHITE
+
+    GRADIENTS = [
+        (128, 128, 128),
+        (160, 160, 160),
+        (192, 192, 192)
+    ]
 
     SIDE_PAD = 100
     TOP_PAD = 150
@@ -66,8 +71,18 @@ def main():
 
 def draw(draw_info):
     draw_info.window.fill(draw_info.BACKGROUND_COLOR)
+    draw_list(draw_info)
     pygame.display.update()
 
+def draw_list(draw_info):
+    lst = draw_info.lst
+
+    for i, val in enumerate(lst):
+        x = draw_info.start_x + i * draw_info.block_width
+        y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
+
+        color = draw_info.GRADIENTS[i % 3]
+        pygame.draw.rect(draw_info.window, color, (x, y, draw_info.block_width, draw_info.height))
 
 if __name__ == "__main__":
     main()
